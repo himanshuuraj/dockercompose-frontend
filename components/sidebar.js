1d99a796c4e41ac4fa8fab8165ebf712281d7d5d
Himanshu <himanshu@spectrocloud.com>
2020-08-29 14:33:21 +0530

    complaint refactores

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,8 +5,7 @@ import { setData } from "./../redux/action";
 import {View, Text, Touch} from "./../ui-kit";
 import { Actions } from 'react-native-router-flux';
 import { Color } from '../global/util';
-const width = Math.round(Dimensions.get('window').width);  
-const height = Math.round(Dimensions.get('window').height);  
+const { width, height } = Dimensions.get('window');  
 
 let arrayOfItems = [
     {id : "MapView", name : "MapView"},
@@ -32,20 +31,15 @@ export default () => {
         ).start();
       }, [])
 
-    const dispatch = useDispatch()
-    const setDataAction = (arg) => dispatch(setData(arg))
+      const dispatch = useDispatch();
+      const setDataAction = (arg) => dispatch(setData(arg));
 
-    let { sidebar, userInfo } = useSelector(state => {
-        return {
-            sidebar: state.testReducer.sidebar,
-            userInfo: state.testReducer.userInfo
-        }
-    }) || {};
+    let { sidebar, userInfo } = useSelector(state => state.testReducer) || {};
 
     if(!sidebar.show)
       return null;
 
-    return <View a le={0} to={0} w={width} h={height} mt={StatusBar.currentHeight} c={'rgba(52, 52, 52, 0.8)'}>
+    return <View a le={0} to={0} w={width} h={height} mt={StatusBar.currentHeight} c={Color.backgroundModalColor}>
         <Animated.View style={{
                 height: '100%',
                 backgroundColor : "#bbb",
--- pages/complaint.js
+++ pages/complaint.js
@@ -3,9 +3,20 @@ import { StatusBar, Dimensions } from "react-native";
 import { View, Text, TextInput, Touch } from "../ui-kit";
 import Header from "../components/header";
 import { Color } from "../global/util";
-let { width, height } = Dimensions.get("window");
+import { useDispatch } from 'react-redux';
+import { setData } from "./../redux/action";
+
+let { width } = Dimensions.get("window");
 
 export default () => {
+
+  const dispatch = useDispatch()
+  const setDataAction = (arg) => dispatch(setData(arg))
+
+  showComplaintMessage = () => {
+    setDataAction({errorModalInfo : { showModal : true, message : "We shall contact you soon." }});
+  }
+
   return (
     <View mt={StatusBar.currentHeight}>
       <Header />
@@ -19,7 +30,7 @@ export default () => {
             <Text t={"Message"} />
             <TextInput nl={2} uc={"#bbb"} ph="Message" nl={4} name={"truckName"} to={10} pb={4} />
           </View>
-          <Touch g jc mt={20} mb={5} h={36} w={'100%'} br={4}
+          <Touch g jc mt={20} mb={5} h={36} w={'100%'} br={4} onPress={showComplaintMessage}
                 s={14} c={Color.white} b t={"SEND"} mb={16} />
         </View>
       </View>
